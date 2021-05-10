15f9497746220221675ca13b964da7bfb426b0bf
Ivan <waizord@yandex.ru>
2021-05-10 23:11:01 +0300

    fix: fixed functin numberConverter and completed exercise-4

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-4/NumberConverter.m
@@ -3,7 +3,26 @@
 @implementation NumberConverter
 
 - (NSArray *)numberConverter:(NSNumber *)number {
-    return nil;
+    NSMutableArray * array = [NSMutableArray new];
+    int inte = [number intValue];
+    if (inte < 0) {
+        inte = inte * (-1);
+    }
+    NSNumber * newNumber = [NSNumber numberWithInt:inte];
+    NSString *string = [newNumber stringValue];
+    NSString * temp = [NSString new];
+    NSInteger i = string.length - 1;
+    if (i >= 0) {
+        for (int j = 0; j <= i; j++) {
+            char s = [string characterAtIndex:j];
+            temp = [NSString stringWithFormat:@"%c",s];
+            [array insertObject:temp atIndex:0];
+        }
+    }else{
+        return [NSArray new];
+    }
+    NSArray * ar = [NSArray arrayWithArray:array];
+    return ar;
 }
 
 @end
