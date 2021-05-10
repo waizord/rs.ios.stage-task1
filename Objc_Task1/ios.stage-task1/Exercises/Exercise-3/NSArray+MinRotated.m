0ad5071bb3435d4df9f1afdcaa89963890c6e926
Ivan <waizord@yandex.ru>
2021-05-10 17:27:43 +0300

    fix: fixed function minRotate and completed exercise-3

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-3/NSArray+MinRotated.m
@@ -3,7 +3,10 @@
 @implementation NSArray (MinRotated)
 
 - (NSNumber *)minRotated {
-    return nil;
+    NSArray * array = [NSArray new];
+    array = [self sortedArrayUsingSelector:@selector(compare:)];
+    NSLog(@"%@", [array firstObject]);
+    return [array firstObject];
 }
 
 @end
