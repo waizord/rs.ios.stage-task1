bef95cca789527facfe4f1141659abb5271fb95f
Ivan <waizord@yandex.ru>
2021-05-10 23:55:40 +0300

    fix: fix exercise-5. Need to change function

--- Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
+++ Objc_Task1/ios.stage-task1/Exercises/Exercise-5/LeaderBoardTracker.m
@@ -10,6 +10,32 @@
 
 - (NSArray <NSNumber *> *)trackLeaderBoard:(NSArray <NSNumber *> *)rankedArray
                                playerArray:(NSArray <NSNumber *> *)playerArray {
+    
+    if (playerArray.count == 0) {
+        return @[];
+    } else {
+        if ((rankedArray.count == 0)) {
+            NSMutableArray * result = [NSMutableArray new];
+            for (int i = 0; i < (int)playerArray.count; i++) {
+                [result addObject:@(1)];
+            }
+            return result;
+        } else {
+            if (rankedArray.count > playerArray.count) {
+                NSMutableArray * result = [NSMutableArray new];
+                for (int i = 0; i < rankedArray.count; i++) {
+                    NSNumber * num = [NSNumber numberWithInt:i];
+                    if (playerArray[i] >= rankedArray[i]) {
+                        [result addObject:num];
+                    } else {
+                        [result addObject:num];
+                    }
+                }
+            }else{
+                
+            }
+        }
+    }
     return @[];
 }
 
